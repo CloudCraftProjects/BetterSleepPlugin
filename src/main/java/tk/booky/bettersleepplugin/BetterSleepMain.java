b9a07f4f0b10261781e232e0cbfbec01206fe360
booky10 <github@booky.tk>
2021-02-28 14:54:58 +0100

    added a listener for entering the bed and start skipping the night

--- src/main/java/tk/booky/bettersleepplugin/BetterSleepMain.java
+++ src/main/java/tk/booky/bettersleepplugin/BetterSleepMain.java
@@ -1,6 +1,8 @@
 package tk.booky.bettersleepplugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import tk.booky.bettersleepplugin.listener.BedEnterListener;
 
 public final class BetterSleepMain extends JavaPlugin {
 
@@ -9,5 +11,7 @@ public final class BetterSleepMain extends JavaPlugin {
     @Override
     public void onEnable() {
         main = this;
+
+        Bukkit.getPluginManager().registerEvents(new BedEnterListener(), this);
     }
 }
---
+++ src/main/java/tk/booky/bettersleepplugin/listener/BedEnterListener.java
@@ -0,0 +1,30 @@
+package tk.booky.bettersleepplugin.listener;
+// Created by booky10 in BetterSleepPlugin (22:35 27.02.21)
+
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;
+import tk.booky.bettersleepplugin.manager.BetterSleepManager;
+import tk.booky.bettersleepplugin.manager.MessageManager;
+
+public class BedEnterListener implements Listener {
+
+    @EventHandler
+    public void onBedEnter(PlayerBedEnterEvent event) {
+        if (event.isCancelled() || !event.getBedEnterResult().equals(PlayerBedEnterEvent.BedEnterResult.OK)) return;
+
+        Player player = event.getPlayer();
+        World world = player.getWorld();
+
+        if (!world.isDayTime() && BetterSleepManager.addSleeping(world.getUID(), player.getUniqueId())) {
+            MessageManager.broadcastWorld(world, MessageManager.SLEEPING_NEEDED, BetterSleepManager.getSleeping(world.getUID()).size(), BetterSleepManager.getSleepingNeeded(world), String.format(MessageManager.SLEEPING_NEEDED_JOIN, player.getName()));
+
+            if (BetterSleepManager.canSkip(world) && !BetterSleepManager.isAlreadySkipping(world.getUID())) {
+                MessageManager.broadcastWorld(world, MessageManager.SLEEPING_START);
+                BetterSleepManager.skipNight(world, (none) -> MessageManager.broadcastWorld(world, MessageManager.NIGHT_OVER));
+            }
+        }
+    }
+}
