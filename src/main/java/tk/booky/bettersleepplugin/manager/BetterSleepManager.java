683aec73b0c46c93c1cfefe0c01871d24e666641
booky10 <github@booky.tk>
2021-02-28 14:45:24 +0100

    definitely not an issue fixed here

--- src/main/java/tk/booky/bettersleepplugin/manager/BetterSleepManager.java
+++ src/main/java/tk/booky/bettersleepplugin/manager/BetterSleepManager.java
@@ -41,7 +41,7 @@ public final class BetterSleepManager {
     }
 
     public static boolean canSkip(World world) {
-        return (double) world.getPlayerCount() / sleeping.size() >= PERCENTAGE;
+        return (double) world.getPlayerCount() / getSleeping(world.getUID()).size() >= PERCENTAGE;
     }
 
     public static BukkitTask skipNight(World world, Consumer<Boolean> consumer) {
