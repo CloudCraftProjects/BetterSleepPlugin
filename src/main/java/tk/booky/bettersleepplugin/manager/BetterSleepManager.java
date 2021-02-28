94feca2515d038fb578bad8eac4ba19e8e73d4e2
booky10 <github@booky.tk>
2021-02-28 14:22:56 +0100

    added a BetterSleepManager, to process all the data

---
+++ src/main/java/tk/booky/bettersleepplugin/manager/BetterSleepManager.java
@@ -0,0 +1,55 @@
+package tk.booky.bettersleepplugin.manager;
+// Created by booky10 in BetterSleepPlugin (22:37 27.02.21)
+
+import org.bukkit.World;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+import tk.booky.bettersleepplugin.BetterSleepMain;
+import tk.booky.bettersleepplugin.utils.AlreadyDayException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import java.util.function.Consumer;
+
+public final class BetterSleepManager {
+
+    private static final List<UUID> sleeping = new ArrayList<>();
+    public static final double PERCENTAGE = 0.3;
+
+    public static boolean isSleeping(UUID uuid) {
+        return sleeping.contains(uuid);
+    }
+
+    public static List<UUID> getSleeping() {
+        return Collections.unmodifiableList(sleeping);
+    }
+
+    public static boolean addSleeping(UUID uuid) {
+        if (!isSleeping(uuid)) return sleeping.add(uuid);
+        else return false;
+    }
+
+    public static boolean removeSleeping(UUID uuid) {
+        return sleeping.remove(uuid);
+    }
+
+    public static boolean canSkip(World world) {
+        return (double) world.getPlayerCount() / sleeping.size() >= PERCENTAGE;
+    }
+
+    public static BukkitTask skipNight(World world, Consumer<Boolean> consumer) {
+        return new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (world.isDayTime() || !canSkip(world)) {
+                    consumer.accept(world.isDayTime());
+                    cancel();
+                } else {
+                    world.setTime(world.getTime() + 100);
+                }
+            }
+        }.runTaskTimer(BetterSleepMain.main, 5, 5);
+    }
+}
