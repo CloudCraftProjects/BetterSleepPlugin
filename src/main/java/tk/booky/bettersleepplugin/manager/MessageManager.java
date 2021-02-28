929c14f6ec938ff94d10215495b06a72ffe67a89
booky10 <github@booky.tk>
2021-02-28 14:41:19 +0100

    added a method for broadcasting messages in specific worlds

--- src/main/java/tk/booky/bettersleepplugin/manager/MessageManager.java
+++ src/main/java/tk/booky/bettersleepplugin/manager/MessageManager.java
@@ -1,7 +1,9 @@
 package tk.booky.bettersleepplugin.manager;
 // Created by booky10 in BetterSleepPlugin (13:27 28.02.21)
 
-public class MessageManager {
+import org.bukkit.World;
+
+public final class MessageManager {
 
     public static final String PREFIX = "§7[§bSleeping§7]§c ";
     public static final String NIGHT_OVER = PREFIX + "§aDie Nacht ist vorbei und es ist nun Tag!";
@@ -10,4 +12,8 @@ public class MessageManager {
     public static final String SLEEPING_NEEDED_LEAVE = "%s ist rausgegangen.";
     public static final String SLEEPING_START = PREFIX + "§aDie Nacht wird übersprungen...";
     public static final String SLEEPING_CANCEL = PREFIX + "§cDas Überspringen der Nacht wurde abgebrochen, da %s aus dem Bett gegangen ist!";
+
+    public static void broadcastWorld(World world, String message, Object... args) {
+        world.getPlayers().forEach(player -> player.sendMessage(String.format(message, args)));
+    }
 }
