5f2e78d3a7e462f490b18e435f998a8bffbbb909
booky10 <github@booky.tk>
2021-02-28 14:19:06 +0100

    basic project structure with a bukkit main class and the plugin.yml

---
+++ src/main/java/tk/booky/bettersleepplugin/BetterSleepMain.java
@@ -0,0 +1,13 @@
+package tk.booky.bettersleepplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterSleepMain extends JavaPlugin {
+
+    public static BetterSleepMain main;
+
+    @Override
+    public void onEnable() {
+        main = this;
+    }
+}
